9aeba116a6ac0f02c587743d7005ee1780ea6177
Weronika Niekludow <118271719+weronika-niekludow@users.noreply.github.com>
2023-04-07 17:52:46 +0200

    Temperature section

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -1,7 +1,31 @@
+import hot from "./assets/hot.jpg";
+import cold from "./assets/cold.jpg";
+
 function App() {
   return (
-    <div className="app">
-      <h1>Hello!</h1>
+    <div className="app" style={{ backgroundImage: `url(${cold})` }}>
+      <div className="overlay">
+        <div className="container">
+          <div className="sectionInputs">
+            <input type="text" name="city" placeholder="Enter City..." />{" "}
+            <button> F </button>
+          </div>
+          <div className="sectionTemperature">
+            <div className="icon">
+              <h3>London,GB</h3>
+              <img
+                src="https://pl.seaicons.com/wp-content/uploads/2022/07/Weather-Clouds-icon.png"
+                alt="weatherIcon"
+              />
+              <h3>Cloudy</h3>
+              <div className="temperature">
+                <h1> 15 C</h1>
+              </div>
+            </div>
+            {/* bottom description */}
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
